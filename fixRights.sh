1aaf5d2189cd310eefd86459e523565ef252f965
tatarinov <alex-dev@yandex.ru>
2018-04-28 11:34:19 +0300

    Исправлена команда установки прав

--- fixRights.sh
+++ fixRights.sh
@@ -1,8 +1,18 @@
-#!bash
+#!/usr/bin/env bash
+
+read -p "Are you sure you fix rigth for directory \"${PWD##*/}\"(y/n)?"
+ 
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+   exit
+fi
+
+
 #права на папки
-find . -type d | xargs chmod 775 {} \;
+find . -type d -exec chmod 775 "{}" \;
 #права все файлы папки
-find . -type f | xargs chmod 664 {} \;
+find . -type f -exec chmod 664 "{}" \;
 chmod 775 index.php
 chmod 775 backend/index.php
 chmod 775 protected/yiic*
+
+echo "complite"
