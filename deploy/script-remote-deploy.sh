3db476d25c3c442a1a5e0d6f27fa21bd798d1760
tatarinov <tatarinov@shogo.ru>
2019-10-28 12:00:04 +0300

    Доработка скрипнов деплоя

--- deploy/script-remote-deploy.sh
+++ deploy/script-remote-deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+COMMANDS=""
+
 if [ -n "$1" ]
 then
   BRANCH="$1"
 else
   echo "Parameter branch not found"
-  exit 1;
+  exit 1
 fi;
 
 if [ -n "$2" ]
@@ -13,23 +15,41 @@ then
   PROJECT_PATH="$2"
 else
   echo "Parameter project_path not found"
-  exit 1;
+  exit 1
 fi;
+
+
+#get and decode commands from argument
+if [ ! -n "$3" ]; then
+ exit 0
+else
+ COMMANDS=`echo $3 | tr '@' '\n\r' | base64 --decode`
+fi
+
+# commands list (example: 'command 1 ; command 2')
+if [ "$COMMANDS" = "" ]; then
+ exit 0
+fi
+
+echo "Executing remote commands..."
 
 cd "$PROJECT_PATH"
 
-CURRENT_BRANCH="$(git branch | grep \* | cut -c 3-)"
+if [[ $COMMANDS == *"git-pull"* ]]; then
 
-if [ "$CURRENT_BRANCH" = "$BRANCH" ]
-then
-  git pull origin "$BRANCH:$BRANCH"
-else
-  git fetch origin "$BRANCH:$BRANCH"
-  git checkout "$BRANCH"
+  #delete git-pull from commnd list
+  COMMANDS=${COMMANDS/\;git-pull/}
+  COMMANDS=${COMMANDS/\git-pull;/}
+  COMMANDS=${COMMANDS/\git-pull/}
+
+  CURRENT_BRANCH="$(git branch | grep \* | cut -c 3-)"
+  if [ "$CURRENT_BRANCH" = "$BRANCH" ]; then
+    git pull origin "$BRANCH:$BRANCH"
+  else
+    git fetch origin "$BRANCH:$BRANCH"
+    git checkout "$BRANCH"
+  fi
 fi
 
-# Оther commands list (example: 'command 1 ; command 2')
-if [ -n "$3" ]
-then
- eval "$3"
-fi;
+eval $COMMANDS
+
