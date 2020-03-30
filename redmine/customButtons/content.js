e539bd6bb834abfee0a240c71a8c569cc13a360a
tatarinov <tatarinov@shogo.ru>
2020-03-30 09:48:33 +0300

    дорабока плагина

--- redmine/customButtons/content.js
+++ redmine/customButtons/content.js
@@ -3,50 +3,66 @@ if( $('#issue_assigned_to_id').val() != '10' )
 
 if( $('#issue_status_id').val() != '2' )
   $('.return-to-author').before('<div class="to-my" title="Взять задачу">Взять задачу</div>');
-else
+else if( isTaskForMy() ) {
   $('.return-to-author').before('<div class="end-task" title="Завершить">Завершить</div>');
+}
+
+$('.settings').closest('li').after('<li><a href="/issues?query_id=222" class="programmer-issue-list" title="Разработка">Разработка</a></li>');
 
 $(document).on('click', '.to-test', function() {
   $('#issue_assigned_to_id').val('10'); // на Ваню
-  $($('input[name=commit]').get(2)).trigger('click');
+  $('#issue_status_id').val('7');  // 1 - Новая 7 - тестирование
+  submitForm();
 });
 
 $(document).on('click', '.to-my', function() {
+  startTimer()
   $('#issue_assigned_to_id').val('6'); // мне
   $('#issue_status_id').val('2'); // выпоняется
-  $($('input[name=commit]').get(2)).trigger('click');
+  submitForm();
 });
 
 $(document).on('click', '.end-task', function() {
-  createTimePopup();
-  //$($('input[name=commit]').get(2)).trigger('click');
+  createTimePopup($(this));
 });
 
-function getTimerValue() {
-  $.get('https://redmine.shogo.ru/time_loggers', function(result){
-    var content = $(result);
-    var time = $(content.find('#user-time-loggers-list .spent-time').get(1)).text();
-    $('#my-time-input').val(time);
+function getTimerValue(callback) {
+  $.ajax({
+    async: true,
+    type: 'GET',
+    url: 'https://redmine.shogo.ru/time_loggers',
+    success: function(result) {
+      var content = $(result);
+      var time = $(content.find('#user-time-loggers-list .spent-time').get(1)).text();
+      callback(time);
+    }
   });
 }
 
-function createTimePopup() {
+function createTimePopup($button) {
 
   if( $('#dialog-message').length == 0)
   {
     $('body').append('<div id="dialog-message" title="Затраченное время"><p><input id="my-time-input" name="my-time"></p><p><textarea cols="2" id="my-comment-input" name="my-comment" placeholder="Комментарий"></textarea></p></div>');
   }
 
-  getTimerValue();
+  getTimerValue(function(time) {
+    $('#my-time-input').val(time);
+  });
+
 
   $( "#dialog-message" ).dialog({
-    modal: true,
+    modal: false,
     buttons: {
       Ok: function() {
         $( this ).dialog( "close" );
         setTime();
+        $('#issue_status_id').val('2'); // выпоняется
+        stopTimer();
+        submitForm();
       }
-    }
+    },
+    position: { my: "left top", at: "left bottom", of: $button }
   });
 
   $( "#issue_notes" ).focus();
@@ -55,5 +71,27 @@ function createTimePopup() {
 function setTime() {
   $('#time_entry_hours').val($('#my-time-input').val());
   $('#issue_notes').val($('#my-comment-input').val());
+}
+
+function submitForm() {
+  $($('input[name=commit]').get(2)).trigger('click');
+}
+
+function isTaskForMy() {
+ return $('#issue_assigned_to_id').val() == 6
+}
+
+function startTimer() {
+  if( $('#time-logger-menu a.icon-start').length )
+    $('#time-logger-menu a.icon-start').trigger('click');
+}
+
+function stopTimer() {
+  $.ajax({
+    async: true,
+    type: 'GET',
+    url: '//redmine.shogo.ru/time_loggers/stop',
+    success: function(result) {    }
+  });
 }
 
